75fbd301013f9a819af12986c19c6060a0b59d9d
Yaros Tashak <yaroslavtashak@gmail.com>
2020-09-18 18:47:12 +0200

    1- For now the game seems to be functional. Probably will need some tweaks in terms of classes set up or related to some methods. Will ask recommendation on the web. 2- Also would like to perform some testings on classes for practice.

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import re
 import string
 
 class Game:
-    def __init__(self):
-        # self.player_name = input("Player name: ")
-        # self.computer_name = "Spoink"
-        self.player_points = 0
-        self.computer_points = 0
-
     @staticmethod
     def check_input(inpt):
         '''
@@ -16,6 +10,7 @@ class Game:
         if re.match("^\((?:[1-9]|0[1-9]|10),\s{0,1}(?:[1-9]|0[1-9]|10)\)$", str(inpt)):
             return True
         else:
+            print("Only integers between 1-10 are valid!")
             return False
 
     @staticmethod
@@ -42,33 +37,56 @@ class Game:
 
                 except ZeroDivisionError:
                     #print("X is zero, need to check if manhattan distance is equal to 1")
-                    print(f"Slope gives error and manhattan is {manhattan}")
+                    #print(f"Slope gives error and manhattan is {manhattan}")
                     return True
 
                 else:
                     if slope == 0:
-                        print(f"Slope is {slope} and manhattan is {manhattan}")
+                        #print(f"Slope is {slope} and manhattan is {manhattan}")
                         return True
 
                     else:
-                        print(slope)
+                        #print(slope)
                         return False
 
             else:
-                print(f"Manhattan distance is not correct")
+                #print(f"Manhattan distance is not correct")
                 return False
         else:
             return True
 
+    @staticmethod
+    def delete_boat_pos(coordinate, target_ships):
+        '''
+            Eliminates a boat from players ships to measure when hit or hit &
+            sunk, also that way we can measure when one player lost.
+        '''
+        for k, v in target_ships.items():
+            if coordinate in v:
+                v.remove(coordinate)
+                if len(v) == 0:
+                    print("HIT & SUNK")
+                else:
+                    print("HIT")
+
+    @staticmethod
+    def player_status(target_ships):
+        '''
+            After a shot, checks if player has any unsunk boats and if he is
+            still able to play, if all boats are sunk, game over.
+        '''
+        flatten_ships = [y for x in target_ships.values() for y in x]
+        if len(flatten_ships) == 0:
+            return True
+        else:
+            return False
 
 
 class Board:
-
-    def __init__(self, columns, rows, player_name):
+    def __init__(self, columns, rows):
         self.rows = rows
         self.columns = columns
         self.board_array = self.board_array(self.rows, self.columns)
-        self.player_name = player_name
 
     def board_array(self, rows, columns):
         '''
@@ -77,7 +95,8 @@ class Board:
         board_array = []
         board_array.append(list(" "))
         board_array[0].append(" ".join(map(str, list(range(1, columns+1)))))
-        for i in range(1, rows+1): #need to add +1 because one row in the board_array belong to ascii characters
+        #need to add +1 because one row in the board_array belongS to ascii characters
+        for i in range(1, rows+1):
             board_array.append(list((f"{ i }", " ".join(" " * rows))))
         return board_array
 
@@ -85,7 +104,6 @@ class Board:
         '''
             Will render the state of a player's board
         '''
-        print(f"{self.player_name}'s' board. ")
         for r in self.board_array:
             print(r)
 
@@ -103,34 +121,28 @@ class Board:
 
         self.render_board()
 
-    def paint_shot(self, ships, shot):
+    def paint_shot(self, guess, shot):
         '''
-            Marks * or @ is hit or miss respectively
+            Marks X or M if hit or miss respectively
         '''
-        #shot = shot[0]
-        if shot in ships:
-            print(f"HIT on {shot}")
-            pos = list(self.board_array[shot[1]][1][::2])
-            pos[shot[0]-1] = "*"
-            pos = " ".join(pos)
-            self.board_array[shot[1]][1] = pos
-            self.render_board()
+        if guess == True:
+            icon = "X"
+        elif guess == False:
+            icon = "M"
 
-        else:
-            print("MISS")
-            pos = list(self.board_array[shot[1]][1][::2])
-            pos[shot[0]-1] = "@"
-            pos = " ".join(pos)
-            self.board_array[shot[1]][1] = pos
-            self.render_board()
+        pos = list(self.board_array[shot[1]][1][::2])
+        pos[shot[0]-1] = icon
+        pos = " ".join(pos)
+        self.board_array[shot[1]][1] = pos
 
 
 
 class Player:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = input("Player name: ").upper()
         self.player_ships = {}
+        self.player_moves = []
 
     def define_ships(self, player_board, ships_settings):
 
@@ -138,7 +150,7 @@ class Player:
             temp_coordinates = []
             for n in range(l):
                 while True:
-                    print(f"Insert one coordinate at a time (eg: 1, 1) for each cell in : {s}")
+                    print(f"Insert one coordinate at a time (eg: 3, 10) for each cell in : {s}")
                     print(f"{n} out of {l}")
                     try:
                         coordinate = tuple(map(int, input().split(",")))
@@ -155,22 +167,47 @@ class Player:
                                 print("Coordinate already assigned to another ship")
                                 continue
                         else:
-                            print("Wrong input", coordinate)
+                            print("Wrong input format", coordinate)
                             continue #will redirect to while loop
                     except (ValueError, ValueError):
                         print(f"Inser comma separated integers. Value is not valid")
 
                 self.player_ships[s] = temp_coordinates
-            print("Setting pegs")
             player_board.set_pegs(self.player_ships[s])
 
-    def perform_shot(self, target_board):
+    def perform_shot(self, player_tracking, target_board, target_ships): #maybe needs a check if shot has been performed previously
+        '''
+            Calls for user input to ger shot coordinates and checks if
+            the coordinates are inside target_ships
+        '''
         while True:
             print(f"Insert one coordinate (eg: 1, 1) to perform a shot")
             try:
                 coordinate = tuple(map(int, input().split(",")))
                 if Game.check_input(coordinate):
-                    target_board.paint_shot(self.player_ships["Carrier"], coordinate)
+                    #checks if shot was executed in previous moves
+                    if coordinate not in self.player_moves:
+                        self.player_moves.append(coordinate)
+                        ships = list(target_ships.values())
+                        flatten_ships = [y for x in ships for y in x]
+                        if coordinate in flatten_ships: #hit
+                            guess = True
+                            Game.delete_boat_pos(coordinate, target_ships)
+                            #player_tracking.paint_shot(guess, flatten_ships, coordinate)
+                            player_tracking.paint_shot(guess, coordinate)
+                            #target_board.paint_shot(guess, flatten_ships, coordinate)
+                            target_board.paint_shot(guess, coordinate)
+                            break
+                        else:
+                            print("MISS")
+                            guess = False
+                            #player_tracking.paint_shot(guess, flatten_ships, coordinate)
+                            player_tracking.paint_shot(guess, coordinate)
+                            #MAYBE SHOULD ALSO MARK A MISS ON PLAYER2 BOARD
+                            break
+                    else:
+                        print("COORDINATE ALREADY USED!")
+
 
             except (ValueError, ValueError):
                 print(f"Inser comma separated integers. Value is not valid")
@@ -178,17 +215,9 @@ class Player:
 
     def auto_boat_input(self, ships, player_board):
         '''
-            Methods to automatically input boats for testing the game quicker
+            Method to automatically input boats for testing the game quicker
         '''
         self.player_ships = ships
 
         for s, l in ships.items():
             player_board.set_pegs(self.player_ships[s])
-
-
-if __name__ == "__main__":
-
-    rows = 10
-    columns = 10
-    ship_type = ["Carrier" , "Battleship", "Cruiser", "Submarine", "Destroyer"]
-    ships_settings= {"Carrier": 5 , "Battleship" : 4, "Cruiser" : 3, "Submarine" : 3, "Destroyer" : 2}
--- run.py
+++ run.py
@@ -4,52 +4,99 @@ from main import Game, Board, Player
 
 rows = 10
 columns = 10
-ship_type = ["Carrier" , "Battleship", "Cruiser", "Submarine", "Destroyer"]
 ships_settings = {"Carrier": 5 , "Battleship" : 4, "Cruiser" : 3, "Submarine" : 3, "Destroyer" : 2}
 
 
-computer_ships = {"Carrier": [(4, 5), (4, 6), (4, 4), (4, 3), (4, 2)],
-                "Battleship" : [(1, 10), (2, 10), (3, 10), (4, 10)],
-                "Cruiser" : [(8, 1), (9, 1), (10, 1)],
-                "Submarine" : [(7, 5), (7, 6), (7, 7)],
-                "Destroyer" : [(9,8), (9,9)]}
-
+#only use in auto_boat_input for quick testing.
+#You can put as many ships as you want, even just one or two ships.
 player_ships = {"Carrier": [(2, 5), (2, 6), (2, 4), (2, 3), (2, 2)],
                 "Battleship" : [(1, 8), (2, 8), (3, 8), (4, 8)],
                 "Cruiser" : [(10, 2), (10, 3), (10, 4)],
                 "Submarine" : [(7, 7), (8, 7), (9, 7)],
-                "Destroyer" : [(10,9), (10,10)]}
+                "Destroyer" : [(9,8), (9,9)]}
 
-for_auto_input = [computer_ships, player_ships]
+player2_ships = {"Carrier": [(3, 5), (3, 6), (3, 4), (3, 3), (3, 2)],
+                "Battleship" : [(2, 8), (3, 8), (4, 8), (5, 8)],
+                "Cruiser" : [(8, 1), (9, 1), (10, 1)],
+                "Submarine" : [(1, 5), (1, 6), (1, 7)],
+                "Destroyer" : [(6,8), (6,9)]}
 
-def main():
+
+def battleship(mode):
+    '''
+        Functions that set ups the game and runs it.
+    '''
     game = Game()
-    player = Player('Boii')
-    computer = Player("Spoink")
-    player_board = Board(rows, columns, player.name)
-    computer_board = Board(rows, columns, computer.name)
+    player = Player()
+    player2 = Player()
 
+    player_board = Board(rows, columns)
+    player_tracking = Board(rows, columns) #board to mark shots
+    player2_board = Board(rows, columns)
+    player2_tracking = Board(rows, columns) #board to mark shots
 
-    player_board.render_board()
+    #quickly creates boats for easier testing
+    #player.auto_boat_input(player_ships, player_board)
     player.define_ships(player_board, ships_settings)
 
-    print(player.player_ships)
+    if mode == 1:
+        player2.auto_boat_input(player2_ships, player2_board)
+    else:
+        player2.define_ships(player2_board, ships_settings)
 
+    #turn taking
+    turn = 1
+    while True:
+        print("\n"*5)
+        if turn == 1: #player1 turn
+            print(f"{player.name}'s MOVE")
 
+            print(f"{player.name}'s TRACKING BOARD")
+            player_tracking.render_board()
+
+            print(f"{player.name}'s PRIMARY BOARD")
+            player_board.render_board()
+
+            player.perform_shot(player_tracking, player2_board, player2.player_ships)
+            #player_tracking.render_board()
+
+            if Game.player_status(player2.player_ships):
+                print(f"GAME OVER, {player.name} WINS")
+                break
+
+            turn -= 1
+
+        elif turn == 0: #player2 round
+            print(f"{player2.name}'s move")
+
+            print(f"{player2.name}'s TRACKING BOARD")
+            player2_tracking.render_board()
+
+            print(f"{player2.name}'s PRIMARY BOARD")
+            player2_board.render_board()
+
+            player2.perform_shot(player2_tracking, player_board, player.player_ships)
+            player2_tracking.render_board()
+
+            if Game.player_status(player.player_ships):
+                print(f"GAME OVER, {player2.name} WINS")
+                break
+            turn += 1
 
 
 if __name__ == "__main__":
-    #main()
-    game = Game()
-    player = Player('Boii')
-    computer = Player("Spoink")
-    player_board = Board(rows, columns, player.name)
-    computer_board = Board(rows, columns, computer.name)
-
-
-    player.auto_boat_input(player_ships, player_board)
-    computer.auto_boat_input(computer_ships, computer_board)
-    print(player.player_ships)
-    print(computer.player_ships)
-    player_board.render_board()
-    computer_board.render_board()
+    print("Pick game mode 1- 1vMachine or 2- 1v1:")
+    while True:
+        mode = input()
+        try:
+            mode = int(mode)
+        except ValueError:
+            print("Write numeric value 1 or 2!")
+            continue
+        if mode == 1: #versus a machine
+            battleship(mode)
+        elif mode == 2: #versus another player
+            battleship(mode)
+        else:
+            print("Only 1 or 2 are valid inputs!")
+            continue
